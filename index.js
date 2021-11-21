c6dad508df172e9304088aa401ad9a63e5ce1fd3
fogelo <antonorlov@MacBook-Pro-Anton.local>
2021-11-22 01:08:55 +0300

    2. вставил картинки с сервера на страницу

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 let a = 5
-$.ajax('https://repetitora.net/api/JS/Images');
-a=8
+$.ajax('https://repetitora.net/api/JS/Images', {
+    success: function (data) {
+        console.log('i got response')
+        console.log(data)
+        data.forEach(element => {
+            let img = document.createElement('img')
+            img.src = element.thumbnail
+            document.body.append(img)
+        });
+
+    }
+});
+a = 8
 console.log(a);
