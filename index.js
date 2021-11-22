efc1b8026197c6eed2043e789f69e223ea3467cf
fogelo <antonorlov@MacBook-Pro-Anton.local>
2021-11-22 14:25:23 +0300

    6. todo-list добавил возможность добавлять таски на сервер и получать их

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-let addBtn = document.querySelector(".add")
+let addBtn = document.querySelector(".addImg")
 let input = document.querySelector('.inp')
-console.log(input)
+let addTask = document.querySelector('.addTask')
+console.log(addTask)
 
 addBtn.addEventListener('click', () => { 
     const promise = getImages(input.value) 
-    promise.then((data)=>{onDataReceived(data)})})
+    promise.then((data)=>{onImagesReceived(data)})})
+
+addTask.addEventListener('click', ()=>{
+    const promise = getTasks() 
+    promise.then((tasks)=>{onTasksReceived(tasks)})})
 
 
-
-function onDataReceived(data) {
-    console.log('i got response')
-    console.log(data)
+function onIamgesReceived(data) {
     data.forEach(element => {
         let img = document.createElement('img')
         img.src = element.thumbnail
@@ -18,3 +20,16 @@ function onDataReceived(data) {
     });
 
 }
+
+function onTasksReceived(tasks) {
+    tasks.forEach(element => {
+        let div = document.createElement('div')
+        div.innerHTML = element;
+        document.body.append(div)
+    });
+
+}
+
+createTask('lern bliatb').then((response)=>{
+    console.log(response)
+})
--- service.js
+++ service.js
@@ -5,3 +5,19 @@ function getImages(pageNumber) {
     })
 
 }
+
+function getTasks() {
+    const promise = axios.get(`https://repetitora.net/api/JS/Tasks?widgetId=220391`);
+    return promise.then((response)=>{
+        return response.data
+    })
+
+}
+
+function createTask(title) {
+    const promise = axios.post(`https://repetitora.net/api/JS/Tasks?widgetId=220391&title=${title}`);
+    return promise.then((response)=>{
+        return response.data
+    })
+
+}
