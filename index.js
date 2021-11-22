3c36f7d662cdb6bc91e0fe7d72617e909e1eab86
fogelo <antonorlov@MacBook-Pro-Anton.local>
2021-11-22 13:26:35 +0300

    5. axios, промис вместо колбэка

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ let addBtn = document.querySelector(".add")
 let input = document.querySelector('.inp')
 console.log(input)
 
-addBtn.addEventListener('click', () => { getImages(input.value, onDataReceived) })
+addBtn.addEventListener('click', () => { 
+    const promise = getImages(input.value) 
+    promise.then((data)=>{onDataReceived(data)})})
+
 
 
 function onDataReceived(data) {
     console.log('i got response')
     console.log(data)
-    console.log('pageNumber: ', pageNumber);
     data.forEach(element => {
         let img = document.createElement('img')
         img.src = element.thumbnail
